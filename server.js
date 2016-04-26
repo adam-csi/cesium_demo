398e4036e9f1ed8f1453669f47a68fe481760cf5
Adam Daube <adam@clearscienceinc.com>
2016-04-26 10:12:13 -0400

    made server.js

---
+++ server.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
+    var server = app.listen('8080', function() {
+        if ('public') {
+            console.log('Cesium development server running publicly.  Connect to http://localhost:%d/', server.address().port);
+        } else {
+            console.log('Cesium development server running locally.  Connect to http://localhost:%d/', server.address().port);
+        }
+    });
+
+    server.on('error', function (e) {
+        if (e.code === 'EADDRINUSE') {
+            console.log('Error: Port %d is already in use, select a different port.', server.port);
+            console.log('Example: node server.js --port %d', server.port + 1);
+        } else if (e.code === 'EACCES') {
+            console.log('Error: This process does not have permission to listen on port %d.', server.port);
+            if (server.port < 1024) {
+                console.log('Try a port number higher than 1024.');
+            }
+        }
+        console.log(e);
+        process.exit(1);
+    });
+
+    server.on('close', function() {
+        console.log('Cesium development server stopped.');
+    });
+
+    var isFirstSig = true;
+    process.on('SIGINT', function() {
+        if (isFirstSig) {
+            console.log('Cesium development server shutting down.');
+            server.close(function() {
+              process.exit(0);
+            });
+            isFirstSig = false;
+        } else {
+            console.log('Cesium development server force kill.');
+            process.exit(1);
+        }
+    });
